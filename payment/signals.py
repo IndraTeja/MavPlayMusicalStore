4dcc5b56374361b7ce0f767b9fca56fc58447442
Jantz021991 <djantz@unomaha.edu>
2017-12-07 23:34:34 -0600

    Removing weasyprint

--- payment/signals.py
+++ payment/signals.py
@@ -4,7 +4,7 @@ from django.core.mail import EmailMessage
 from django.conf import settings
 from paypal.standard.models import ST_PP_COMPLETED
 from paypal.standard.ipn.signals import valid_ipn_received
-import weasyprint
+# import weasyprint
 from io import BytesIO
 
 from orders.models import Order
@@ -23,18 +23,18 @@ def payment_notification(sender, **kwargs):
         # create invoice e-mail
         subject = 'MavPlay Musical Store - Invoice nr. {}'.format(order.id)
         message = 'Please, find attached the invoice for your recent purchase.'
-        email = EmailMessage(subject, message, 'admin@myshop.com', [order.email])
+        email = EmailMessage(subject, message, 'isdfall2017team1@gmail.com', [order.email])
 
         # generate PDF
         html = render_to_string('orders/order/pdf.html', {'order': order})
         out = BytesIO()
-        stylesheets=[weasyprint.CSS(settings.STATIC_ROOT + 'css/pdf.css')]
-        weasyprint.HTML(string=html).write_pdf(out,
-                                               stylesheets=stylesheets)
-        # attach PDF file
-        email.attach('order_{}.pdf'.format(order.id),
-                     out.getvalue(),
-                     'application/pdf')
+        # # stylesheets=[weasyprint.CSS(settings.STATIC_ROOT + 'css/pdf.css')]
+        # # weasyprint.HTML(string=html).write_pdf(out,
+        #                                        stylesheets=stylesheets)
+        # # attach PDF file
+        # email.attach('order_{}.pdf'.format(order.id),
+        #              out.getvalue(),
+        #              'application/pdf')
         # send e-mail
         email.send()
 
